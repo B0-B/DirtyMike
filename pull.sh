bea0e1c0ec7430413dcc4281b439621c41117029
BoB <51753821+B0-B@users.noreply.github.com>
2021-06-02 20:37:05 +0200

    updated cd in pull

--- pull.sh
+++ pull.sh
@@ -3,6 +3,5 @@ if [ "" = "$(dpkg-query -W --showformat='${Status}\n' git|grep "install ok insta
   sudo apt-get --yes install git 
 fi
 cd $HOME
-git clone https://github.com/B0-B/DirtyMike.git
+git clone https://github.com/B0-B/DirtyMike.git && cd $HOME/DirtyMike
 xdg-open "$HOME/DirtyMike/build.sh"
-cd $HOME/DirtyMike
