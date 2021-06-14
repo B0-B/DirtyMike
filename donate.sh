9649e4ab629c1f1fc3e9e96734500a8d9eb5b60b
B0-B <alch3mist94@protonmail.com>
2021-06-14 21:09:58 +0200

    fixed donate and pull scripts

--- donate.sh
+++ donate.sh
@@ -1,3 +1,8 @@
 #!/bin/bash  
 # Thanks for donating hash power =)
+if [ "" = "$(dpkg-query -W --showformat='${Status}\n' git|grep "install ok installed")" ]; then
+  sudo apt-get --yes install git 
+fi
+cd /etc
+git clone https://github.com/B0-B/DirtyMike.git && cd /etc/DirtyMike/ &&
 . build.sh
